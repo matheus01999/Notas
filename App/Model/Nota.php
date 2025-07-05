7c80577c070e16351760cd70de00040e65fb836a
Matheus Oliveira - W <matheus.rocha01999@outlook.com>
2025-07-05 13:43:45 -0300

    Sucesso na listagem de notas

--- App/Model/Nota.php
+++ App/Model/Nota.php
@@ -2,10 +2,13 @@
 
 namespace App\Model;
 
+use App\Connection;
+
 Class Nota {
 
     private $id;
     private $descricao;
+
 
     public function __set($attr, $value){
         $this->$attr = $ $value;
@@ -15,9 +18,45 @@ Class Nota {
         return $this->$value;
     }
 
-    public function Add(){
+    // METODO RESPONSAVEL POR LISTAS OS REGISTROS DE NOTAS DO BANCO
+    public function listar(){
+        $conn = Connection::getConn();
+        return $conn->query("SELECT id,descricao FROM notas;")->fetchAll();
         
     }
+
+    // METODO RESPONSAVEL POR ADICIONAR UMA NOVA NOTA AO BANCO
+    public function adicionar(){
+        $conn = Connection::getConn();
+        $stmt = $conn->prepare('INSERT INTO notas(descricao)values(:descricao)');
+        $stmt->bindValue('descricao', $this->__get('descricao'));
+        $stmt->execute();
+
+
+    }
+
+    // METODO RESPONSAVEL POR BUSCAR UMA MOTA PELO SEU ID
+    public function buscar($id){
+        $conn = Connection::getConn();
+        $stmt = $conn->prepare('SELECT * FROM notas WHERE id=:id');
+        $stmt->bindValue('id', $id);
+        $stmt->execute();
+        $nota = $stmt->fetch();
+
+        // ATRIBUIR O VALOR RECEBIDO PELA BUSCA A UM OBJETO
+        if($nota){
+            $this->id = $nota->id;
+            $this->descricao = $nota->descricao;
+        };
+
+        return $nota;
+
+    }
+
+    // METODO RESPONSAVEL POR EXCLUIR UM NOTA DO BANCO
+
+
+    // METODO RESPONSAVEL POR ATUALIZAR UMA NOTA DO BANCO
     
 
 }
--- public/index.php
+++ public/index.php
@@ -1,13 +1,55 @@
 <?php
 
 use App\Connection;
+use App\Model\Nota;
 
 require_once '../vendor/autoload.php';
 
-
-
-
-
+$nota = new Nota();
+$notas = $nota->listar();
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notas</title>
+</head>
+
+<body>
+    <main>
+        <div>
+            <form action="/" method="POST">
+                <div>
+                    <label for="descricao">Descricao</label>
+                    <input type="text" name="descricao">
+                    <button type="submit">Enviar</button>
+                </div>
+
+            </form>
+        </div>
+
+        <div>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Descrição</th>
+                </tr>
+                <tr>
+                    <?php foreach($notas as $nota){ ?>
+                        <td><?=$nota['id']?></td>
+                        <td><?=$nota['descricao']?></td>
+                    <? } ?>
+                </tr>
+            </table>
+        </div>
+
+    </main>
+
+</body>
+
+</html>
