777ed0e0fed72c7730dcdf2b8a5dc2698de0fffd
Matheus Oliveira - W <matheus.rocha01999@outlook.com>
2025-07-07 12:23:22 -0300

    estilo da pagina ajustado

--- public/index.php
+++ public/index.php
@@ -18,41 +18,41 @@ $notas = $nota->listar();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notas</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/estilo.css">
 </head>
 
 <body>
     <main>
-        <div class="container">
-            <form action="/" method="POST" class="form-control">
-                <div class="col">
-                    <div class="row">
-                        <label for="descricao" class="form-label ">Descricao</label>
-                        <input type="text" name="descricao" class="form-control">
-                        <button type="submit" class="btn btn-primary form-control mt-4">Enviar</button>
+        <div class="gridContainer">
+            <div class="Formulario">
+                <h3>Adicione uma nova nota</h3>
+                <form action="#">
+                    <div class="mb-3">
+                        <label class="form-label">Descrição</label><br>
+                        <input type="text" class="form-control">
                     </div>
-                </div>
 
-            </form>
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </form>
+            </div>
+
+            <div class="Notas">
+                <h3>Notas Salvas</h3>
+                <table class="table">
+                    <tr>
+                        <th>ID</th>
+                        <th>Nota</th>
+                        <th>Status</th>
+                    </tr>
+                </table>
+            </div>
         </div>
 
-        <div>
-            <table class='table'>
-                <tr>
-                    <th>ID</th>
-                    <th>Descrição</th>
-                </tr>
-                <tr>
-                    <?php foreach ($notas as $nota) { ?>
-                        <td><?= $nota['id'] ?></td>
-                        <td><?= $nota['descricao'] ?></td>
-                    <? } ?>
-                </tr>
-            </table>
-        </div>
 
     </main>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
